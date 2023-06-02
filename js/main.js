745a710ff8fa62afc2d1c5888f263d1c680fc7b8
Elizabet <elinech.2002@gmail.com>
2023-06-02 19:42:28 +0200

    add contacts and edit all file

--- js/main.js
+++ js/main.js
@@ -37,3 +37,66 @@ secondFilmRef.set({
   category: "komedie",
   tags: ["forest", "filmy", "onlyhere"]
 });
+var thirdFilmRef = ref.push();
+thirdFilmRef.set({
+  title: "Joe Black",
+  description: "Śmierć pod pseudonimem Joe Black zjawia się po Williama. Nieoczekiwanie zakochuje się w jego córce.",
+  rezyseria: "Martin Brest",
+  scenariusz: "Ron Osborn, Jeff Reno",
+  gatunek: "Fantasy",
+  produkcja: "USA",
+  price: {
+    original: "70,00 zł",
+    discounted: "65,00 zł"
+  },
+  sku: "AA673",
+  category: "fantasy",
+  tags: ["fantasy", "filmy", "smok"]
+});
+var thirdFilmRef = ref.push();
+thirdFilmRef.set({
+  title: "Jak wytresować smoka 2",
+  description: "Pięć lat po zjednoczeniu rasy smoków oraz ludzi Czkawka i Szczerbatek stają do obrony wyspy Berk przed niebezpiecznymi dzikimi bestiami, a także tajemniczym Smoczym Jeźdźcem.",
+  rezyseria: "Dean DeBlois",
+  scenariusz: "Dean DeBlois",
+  gatunek: "Fantasy",
+  produkcja: "USA",
+  price: {
+    original: "50,00 zł"
+  },
+  sku: "AK124",
+  category: "fantasy",
+  tags: ["fantasy", "miłość", "joe"]
+});
+var fifthFilmRef = ref.push();
+thirdFilmRef.set({
+  title: "Avatar",
+  description: "Jake, sparaliżowany były komandos, zostaje wysłany na planetę Pandora, gdzie zaprzyjaźnia się z lokalną społecznością i postanawia jej pomóc.",
+  rezyseria: "James Cameron",
+  scenariusz: "James Cameron",
+  gatunek: "Przygoda",
+  produkcja: "USA, Wielka Brytania",
+  price: {
+    original: "130,00 zł",
+    discounted: "120,00 zł"
+  },
+  sku: "AN651",
+  category: "przygoda",
+  tags: ["avatar", "miłość", "new"]
+});
+var sixthFilmRef = ref.push();
+thirdFilmRef.set({
+  title: "Forsage",
+  description: "Dziesiąty film sagi „Szybcy i wściekli” rozpoczyna ostatni rozdział jednej z najbardziej popularnych globalnych franczyz, która trwa już trzecią dekadę i ma niesłabnącą popularność. W ciągu wielu misji i wbrew przeciwnościom losu Dom Toretto (Vin Diesel) i jego rodzina przechytrzyli i prześcignęli każdego wroga na swojej drodze. Teraz muszą zmierzyć się z najgroźniejszym przeciwnikiem, z jakim kiedykolwiek mieli do czynienia.",
+  rezyseria: "Louis Leterrier",
+  scenariusz: "Dan Mazeau, Justin Lin",
+  gatunek: "Przygoda",
+  produkcja: "USA",
+  price: {
+    original: "150,00 zł",
+    discounted: "135,00 zł"
+  },
+  sku: "AN343",
+  category: "przygoda",
+  tags: ["przygoda", "rodzina", "forsage"]
+});
